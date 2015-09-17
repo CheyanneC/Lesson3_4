9c5d40fbe5f4c350ad9758e94074793f96b9db95
chey2540 <chey2540@ISS147D8-01.scdsb.pri>
2015-09-17 10:37:33 -0400

    Started PowerCharts

--- src/PowerCharts.java
+++ src/PowerCharts.java
@@ -1,9 +1,28 @@
 import hsa.*;
+import BreezySwing.Format;
 
 public class PowerCharts 
 {
     public static void main(String[] args) 
     {
+        Console c = new Console();
         
+        int exp;
+        
+        while(true)
+        {
+            c.print("Enter an exponent value between 0 and 10: ");
+            exp = c.readInt();
+            
+            if(exp >= 0 && exp <= 10) break;
+        }
+        
+        c.print(Format.justify('c', "X", 5));
+        c.print(Format.justify('c', "2^X", 10));
+        
+        for(int x = 0; x <= exp; x ++)
+        {
+            
+        }
     }
 }
