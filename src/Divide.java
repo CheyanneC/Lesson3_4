1c3fb8120990c6ed15ee487725d15eeec504056e
chey2540 <chey2540@ISS147D8-01.scdsb.pri>
2015-09-17 09:53:25 -0400

    Notes added to divide

--- src/Divide.java
+++ src/Divide.java
@@ -8,25 +8,25 @@ public class Divide
         
         int num1, num2;
         
-        c.print("Enter number 1: ");
+        c.print("Enter number 1: "); //ask for user's numbers
         num1 = c.readInt();
         c.print("Enter number 2: ");
         num2 = c.readInt();
         
-        int div, rem;
+        int div, rem; //division and remainder
         
-        if(num1 > num2)
+        if(num1 > num2) //if num1 is bigger withdraw num2 from it
         {
             div = num1/num2;
             rem = num1 % num2;
         }
-        else
+        else //else (num2 bigger) withdraw num1 from num2
         {
             div = num2/ num1;
             rem = num2%num1;
         }
         
-        c.print("\nThe quotient is " + div);
+        c.print("\nThe quotient is " + div); //print the answers
         c.print("\nThe remainder is " + rem);
     }
     
