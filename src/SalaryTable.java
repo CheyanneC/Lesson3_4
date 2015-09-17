a802f103c8a91cee47ed856ad1c398ca6910f520
chey2540 <chey2540@ISS147D8-01.scdsb.pri>
2015-09-17 09:51:25 -0400

    SalaryTable written in console format

--- src/SalaryTable.java
+++ src/SalaryTable.java
@@ -1,10 +1,24 @@
-
+import TurtleGraphics.*;
+import hsa.*;
 
 public class SalaryTable 
 {
     public static void main(String[] args) 
     {
+        Console c = new Console();
         
+        double old = 0, raise = 40000, pay = 40000;
+        
+        c.println(old + "\t" + raise + "\t" + pay);
+        
+        for(int x = 0; x <= 8; x ++)
+        {
+            old = pay;
+            raise = pay * 0.03;
+            pay = pay + raise;
+            
+            c.print("\n");
+            c.println(old + "\t" + raise + "\t" + pay);
+        }
     }
-    
 }
