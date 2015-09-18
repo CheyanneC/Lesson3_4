1b1eb78cdd7e8733fd759945881276e742a0d7ab
chey2540 <chey2540@ISS147D8-01.scdsb.pri>
2015-09-18 10:11:24 -0400

    Comments added to SalaryTable

--- src/SalaryTable.java
+++ src/SalaryTable.java
@@ -7,28 +7,29 @@ public class SalaryTable
     {
         Console c = new Console();
         
-        double old = 0, raise = 40000, pay = 40000;
+        double old = 0, raise = 40000, pay = 40000; //start the numbers its amount
         
-        c.print(Format.justify('c', "YEAR", 6));
+        c.print(Format.justify('c', "YEAR", 6)); //declare the columns
         c.print(Format.justify('r', "OLD SALARY", 20));
         c.print(Format.justify('r', "RAISE", 20));
         c.println(Format.justify('r', "NEW SALARY\n", 20));
         
         for(int year = 0; year <= 9; year ++)
         {
-            c.print(Format.justify('c', (year + 1), 6));
+            c.print(Format.justify('c', (year + 1), 6)); //print the year
             
-            old = pay;
+            old = pay; //collect the old salary
             
-            c.print(Format.justify('r', old, 20, 2));
+            c.print(Format.justify('r', old, 20, 2)); //print old salary
             
-            raise = pay * 0.03;
+            raise = pay * 0.03; //find out the raise
             
-            c.print(Format.justify('r', raise, 20, 2));
+            c.print(Format.justify('r', raise, 20, 2)); //print raise
             
-            pay += raise;
+            pay += raise; //find out new salary
             
-            c.println(Format.justify('r', pay, 20, 2));
+            c.println(Format.justify('r', pay, 20, 2)); //print salary
+            //repeat until all 10 years are done
         }
     }
 }
