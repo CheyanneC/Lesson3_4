a8c420455feffbf74f7fae3f354d60672d2e2513
chey2540 <chey2540@ISS147D8-01.scdsb.pri>
2015-09-18 09:51:35 -0400

    PowerCharts sort of working

--- src/PowerCharts.java
+++ src/PowerCharts.java
@@ -3,12 +3,12 @@ import BreezySwing.Format;
 
 public class PowerCharts 
 {
+    static int exp;
+    
     public static void main(String[] args) 
     {
         Console c = new Console();
-        
-        int exp;
-        
+
         while(true)
         {
             c.print("Enter an exponent value between 0 and 10: ");
@@ -18,11 +18,28 @@ public class PowerCharts
         }
         
         c.print(Format.justify('c', "X", 5));
-        c.print(Format.justify('c', "2^X", 10));
+        c.print(Format.justify('c', "2^X\n", 10));
+        
+        int ans = 0;
         
         for(int x = 0; x <= exp; x ++)
         {
-            
-        }
+            c.print(Format.justify('c', x , 5));
+            c.print(Format.justify('c', raiseIntPower(x), 10));
+        }        
     }
+    
+    static private int raiseIntPower (int x)
+    {
+        int total = 0;
+        
+        for(int n = 0; n <= 10; n++)
+        {
+            total = Math.pow(x, exp);
+        } 
+
+        return total; 
+    
+    }
+}
 }
